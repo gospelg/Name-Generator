b9c86b6fce5bc5c44fe5265ba647126337d1c3b9
gospelg <garrettcrosby@outlook.com>
2017-01-04 12:01:28 -0500

    Version 1.2
    Added the main learning functions, updated to work with importing the base module

--- main.py
+++ main.py
@@ -1,72 +1,180 @@
-#gematria calculator
-
-#assigning a number to a letter
-alphabet_dict = {
-	1:'a', 
-	2:'b', 
-	3:'c', 
-	4:'d', 
-	5:'e', 
-	6:'f', 
-	7:'g', 
-	8:'h', 
-	9:'i', 
-	10:'j', 
-	11:'k', 
-	12:'l',
-	13:'m',
-	14:'n',
-	15:'o',
-	16:'p',
-	17:'q',
-	18:'r',
-	19:'s',
-	20:'t',
-	21:'u',
-	22:'v',
-	23:'w',
-	24:'x',
-	25:'y',
-	26:'z'
-}
-
-#this will find the number that corresponds to the word
-def find_number(word):
-	
-	number = 0
-	for i in word:
-		for key, value in alphabet_dict.items():
-			if value == i:
-				number += key
-			
-	return number
+import base
 
 #this function accounts for the first letter of a word. It is dependent on the first_letter dictionary in base weights.py
 def first_char(place_names):
 	for place in place_names:
 		i = place[0]
 		if i in first_letter:
-			first_letter[i] = first_letter[i] + 1
+			base.first_letter[i] = base.first_letter[i] + 1
+			
+
+#this is the primary learning function. It takes account for each letter in each word fed into it, and records the frequency
+#of those letters in relation to other letters.
+def learn_language(place_names):
+
+	for place in place_names:
+		for ii in range(1, len(place) - 1):
+			find_frequency(place, ii)
+		
+		
+#this one identifies the letter, selects the correct dictionary and adds values.		
+def find_frequency(place, xx):
+	char = place[xx]
+	#if xx == len(place):
+	#	break
+	
+	if char == 'a':
+		nex_char = place[xx + 1]
+		base.a[nex_char] = base.a[nex_char] + 1
+		base.a['total'] = base.a['total'] + 1
+			
+	if char == 'b':
+		nex_char = place[xx + 1]
+		base.b[nex_char] = base.b[nex_char] + 1
+		base.b['total'] = base.b['total'] + 1
+			
+	if char == 'c':
+		nex_char = place[xx + 1]
+		base.c[nex_char] = base.c[nex_char] + 1
+		base.c['total'] = base.c['total'] + 1
+		
+	if char == 'd':
+		nex_char = place[xx + 1]
+		base.d[nex_char] = base.d[nex_char] + 1
+		base.d['total'] = base.d['total'] + 1
+		
+	if char == 'e':
+		nex_char = place[xx + 1]
+		base.e[nex_char] = base.e[nex_char] + 1
+		base.e['total'] = base.e['total'] + 1
+	
+	if char == 'f':
+		nex_char = place[xx + 1]
+		base.f[nex_char] = base.f[nex_char] + 1
+		base.f['total'] = base.f['total'] + 1
+		
+	if char == 'g':
+		nex_char = place[xx + 1]
+		base.g[nex_char] = base.g[nex_char] + 1
+		base.g['total'] = base.g['total'] + 1
+		
+	if char == 'h':
+		nex_char = place[xx + 1]
+		base.h[nex_char] = base.h[nex_char] + 1
+		base.h['total'] = base.h['total'] + 1
+		
+	if char == 'i':
+		nex_char = place[xx + 1]
+		base.i[nex_char] = base.i[nex_char] + 1
+		base.i['total'] = base.i['total'] + 1
+		
+	if char == 'j':
+		nex_char = place[xx + 1]
+		base.j[nex_char] = base.j[nex_char] + 1
+		base.j['total'] = base.j['total'] + 1
+	
+	if char == 'k':
+		nex_char = place[xx + 1]
+		base.k[nex_char] = base.k[nex_char] + 1
+		base.k['total'] = base.k['total'] + 1
+		
+	if char == 'l':
+		nex_char = place[xx + 1]
+		base.l[nex_char] = base.l[nex_char] + 1
+		base.l['total'] = base.l['total'] + 1
+		
+	if char == 'm':
+		nex_char = place[xx + 1]
+		base.m[nex_char] = base.m[nex_char] + 1
+		base.m['total'] = base.m['total'] + 1	
+		
+	if char == 'n':
+		nex_char = place[xx + 1]
+		base.n[nex_char] = base.n[nex_char] + 1
+		base.n['total'] = base.n['total'] + 1		
+	
+	if char == 'o':
+		nex_char = place[xx + 1]
+		base.o[nex_char] = base.o[nex_char] + 1
+		base.o['total'] = base.o['total'] + 1
+		
+	if char == 'p':
+		nex_char = place[xx + 1]
+		base.p[nex_char] = base.p[nex_char] + 1
+		base.p['total'] = base.p['total'] + 1
+		
+	if char == 'q':
+		nex_char = place[xx + 1]
+		base.q[nex_char] = base.q[nex_char] + 1
+		base.q['total'] = base.q['total'] + 1	
+		
+	if char == 'r':
+		nex_char = place[xx + 1]
+		base.r[nex_char] = base.r[nex_char] + 1
+		base.r['total'] = base.r['total'] + 1
+		
+	if char == 's':
+		nex_char = place[xx + 1]
+		base.s[nex_char] = base.s[nex_char] + 1
+		base.s['total'] = base.s['total'] + 1	
+		
+	if char == 't':
+		nex_char = place[xx + 1]
+		base.t[nex_char] = base.t[nex_char] + 1
+		base.t['total'] = base.t['total'] + 1	
+	
+	if char == 'u':
+		nex_char = place[xx + 1]
+		base.u[nex_char] = base.u[nex_char] + 1
+		base.u['total'] = base.u['total'] + 1	
+		
+	if char == 'v':
+		nex_char = place[xx + 1]
+		base.v[nex_char] = base.v[nex_char] + 1
+		base.v['total'] = base.v['total'] + 1	
+		
+	if char == 'w':
+		nex_char = place[xx + 1]
+		base.w[nex_char] = base.w[nex_char] + 1
+		base.w['total'] = base.w['total'] + 1	
+		
+	if char == 'x':
+		nex_char = place[xx + 1]
+		base.x[nex_char] = base.x[nex_char] + 1
+		base.x['total'] = base.x['total'] + 1		
+		
+	if char == 'y':
+		nex_char = place[xx + 1]
+		base.y[nex_char] = base.y[nex_char] + 1
+		base.y['total'] = base.y['total'] + 1		
+		
+	if char == 'z':
+		nex_char = place[xx + 1]
+		base.z[nex_char] = base.z[nex_char] + 1
+		base.z['total'] = base.z['total'] + 1		
+		
+	if char == ' ':
+		nex_char = place[xx + 1]
+		base.blank[nex_char] = base.blank[nex_char] + 1
+		base.blank['total'] = base.blank['total'] + 1		
+		
+	else:
+		pass
+		
+		
+		
+		
+def first_char(place_names):
+	for place in place_names:
+		i = place[0]
+		if i in base.first_letter.items():
+						
 	
 			
 		
 		
 		
-#notes
-#here is an example function that returns all permutations of a list. Seems useful
-#def permutation(s):
-#   if len(s) == 1: this first if statement just accounts for a list having one item, in which case it has only one permutation
-#     return [s]
-#
-#   perm_list = [] # resulting list
-#   for a in s:
-#     remaining_elements = [x for x in s if x != a] identifies the rest character. It cycles for a in s and then remaining list is everything that is not a
-#     z = permutation(remaining_elements) # permutations of sublist
-#
-#    for t in z:
-#       perm_list.append([a] + t)
-#
-#   return perm_list
+
 		
 	
 	
