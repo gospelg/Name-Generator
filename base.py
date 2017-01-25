76daf13c8dc4818c3d7c6a50709383be1efa1088
gospelg <garrettcrosby@outlook.com>
2017-01-25 12:55:06 -0500

    changed syntax

--- base.py
+++ base.py
@@ -868,33 +868,5 @@ first_letter = {
 	'total':0,
 	}
 
-all_dicts = [
-	a,
-	b, 
-	c, 
-	d, 
-	e, 
-	f, 
-	g, 
-	h, 
-	i, 
-	j, 
-	k, 
-	l, 
-	m, 
-	n, 
-	o, 
-	p, 
-	q, 
-	r, 
-	s, 
-	t, 
-	u, 
-	v, 
-	w, 
-	x, 
-	y, 
-	z, 
-	blank, 
-	first letter]
+all_dicts = [a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z, blank, first letter]
 	
