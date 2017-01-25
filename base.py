6f174775727049a9bdb298e6df0daad4c774c55b
gospelg <garrettcrosby@outlook.com>
2017-01-25 12:51:42 -0500

    added tuple of all dictionaries

--- base.py
+++ base.py
@@ -868,3 +868,33 @@ first_letter = {
 	'total':0,
 	}
 
+all_dicts = [
+	a,
+	b, 
+	c, 
+	d, 
+	e, 
+	f, 
+	g, 
+	h, 
+	i, 
+	j, 
+	k, 
+	l, 
+	m, 
+	n, 
+	o, 
+	p, 
+	q, 
+	r, 
+	s, 
+	t, 
+	u, 
+	v, 
+	w, 
+	x, 
+	y, 
+	z, 
+	blank, 
+	first letter]
+	
