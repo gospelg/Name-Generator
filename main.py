9897792935889926d938a27727ee126001a55580
gospelg <garrettcrosby@outlook.com>
2017-03-31 12:25:56 -0400

    Version 0.2
    Took out weights function, it wont be needed here

--- main.py
+++ main.py
@@ -2,40 +2,9 @@
 from __future__ import division
 import json
 import os
-from learner import alpha_list
+from base import alpha_list
 import numpy
 
-alpha_index = [
-    "blank", 
-    "a", 
-    "b", 
-    "c", 
-    "d", 
-    "e", 
-    "f", 
-    "g", 
-    "h", 
-    "i", 
-    "j", 
-    "k", 
-    "l", 
-    "m", 
-    "n", 
-    "o", 
-    "p", 
-    "q", 
-    "r", 
-    "s", 
-    "t", 
-    "total", 
-    "u", 
-    "v", 
-    "w", 
-    "x", 
-    "y", 
-    "z"
-    ]
-	
 
 #imports json data back into dictionaries. The arguement is the dir where the specific jsons are, to be inputed by user in another 
 #function.
@@ -48,21 +17,12 @@ def import_data(file_path):
 	    with open(filename, 'r') as f:
 		    data_dump.append(json.load(f))
 	return data_dump
-		
-def weights(the_data):
-	for dataset in the_data:
-		for i in [x for x in range(27) if x != 21]:
-			if dataset[alpha_index[i]] != 0:
-				dataset[alpha_index[i]] = dataset[alpha_index[i]] / dataset['total']
-			else:
-				pass
-			
-			
+				
 def main():
-	file_path = raw_input('Where are the jsons?')
+	working_dir = os.path.dirname(os.path.abspath( __file__ ))
+    file_path = raw_input('Where are the jsons?')
 	language = import_data(file_path)
-	weights(language)
-	print language
+	
 	
 main()
 	
