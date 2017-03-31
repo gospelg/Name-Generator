53b0979d43bf2c7f1abe711b5ef95122a7369de5
gospelg <garrettcrosby@outlook.com>
2017-03-31 18:01:06 -0400

    version 1
    Kinda works? The while loop is what I suspect is the problem.

--- main.py
+++ main.py
@@ -7,14 +7,14 @@ import numpy as np
 #imports json data back into dictionaries. The arguement is the dir where the specific jsons are, to be inputed by user in another 
 #function.
 def import_data(file_path):
-	alphabet = alpha_list()
-	data_dump = []
+    alphabet = alpha_list()
+    data_dump = []
 #files are loaded as dictionaries to an array. They are loaded in order, and must be accessed by index. 
-	for i in range(len(alphabet)):
-	    filename = file_path + '\%s.json' % alphabet[i]
-	    with open(filename, 'r') as f:
-		    data_dump.append(json.load(f))
-	return data_dump
+    for i in range(len(alphabet)):
+        filename = file_path + '\%s.json' % alphabet[i]
+        with open(filename, 'r') as f:
+            data_dump.append(json.load(f))
+    return data_dump
 
 def data_strip(data):
     prob = []
@@ -30,184 +30,193 @@ def generate_name(dataset):
     mu = stats["mean"]
     std = stats["std_dev"]
     word_length = np.random.normal(mu, std)
+    print word_length
     
     first = data_strip(dataset[27])
     x = np.random.choice(first[0], p=first[1])
     place.append(x.upper())
     
     while len(place) < word_length:
-		if char == 'A':
-			stuff = data_strip(dataset[0])
-			y = np.random.choice(stuff[0], p=stuff[1])
-			place.append(y.lower())
-			x = y
-		
-		if char == 'B':
-			stuff = data_strip(dataset[1])
-			y = np.random.choice(stuff[0], p=stuff[1])
-			place.append(y.lower())
-			x = y
-				
-		if char == 'C':
-			stuff = data_strip(dataset[2])
-			y = np.random.choice(stuff[0], p=stuff[1])
-			place.append(y.lower())
-			x = y
-			
-		if char == 'D':
-			stuff = data_strip(dataset[3])
-			y = np.random.choice(stuff[0], p=stuff[1])
-			place.append(y.lower())
-			x = y
-			
-		if char == 'E':
-			stuff = data_strip(dataset[4])
-			y = np.random.choice(stuff[0], p=stuff[1])
-			place.append(y.lower())
-			x = y
-		
-		if char == 'F':
-			stuff = data_strip(dataset[5])
-			y = np.random.choice(stuff[0], p=stuff[1])
-			place.append(y.lower())
-			x = y
-			
-		if char == 'G':
-			stuff = data_strip(dataset[6])
-			y = np.random.choice(stuff[0], p=stuff[1])
-			place.append(y.lower())
-			x = y
-			
-		if char == 'H':
-			stuff = data_strip(dataset[7])
-			y = np.random.choice(stuff[0], p=stuff[1])
-			place.append(y.lower())
-			x = y
-			
-		if char == 'I':
-			stuff = data_strip(dataset[8])
-			y = np.random.choice(stuff[0], p=stuff[1])
-			place.append(y.lower())
-			x = y
-			
-		if char == 'J':
-			stuff = data_strip(dataset[9])
-			y = np.random.choice(stuff[0], p=stuff[1])
-			place.append(y.lower())
-			x = y
-		
-		if char == 'K':
-			stuff = data_strip(dataset[10])
-			y = np.random.choice(stuff[0], p=stuff[1])
-			place.append(y.lower())
-			x = y
-			
-		if char == 'L':
-			stuff = data_strip(dataset[11])
-			y = np.random.choice(stuff[0], p=stuff[1])
-			place.append(y.lower())
-			x = y
-			
-		if char == 'M':
-			stuff = data_strip(dataset[12])
-			y = np.random.choice(stuff[0], p=stuff[1])
-			place.append(y.lower())
-			x = y
-			
-		if char == 'N':
-			stuff = data_strip(dataset[13])
-			y = np.random.choice(stuff[0], p=stuff[1])
-			place.append(y.lower())
-			x = y      
-		
-		if char == 'O':
-			stuff = data_strip(dataset[14])
-			y = np.random.choice(stuff[0], p=stuff[1])
-			place.append(y.lower())
-			x = y
-			
-		if char == 'P':
-			stuff = data_strip(dataset[15])
-			y = np.random.choice(stuff[0], p=stuff[1])
-			place.append(y.lower())
-			x = y
-			
-		if char == 'Q':
-			stuff = data_strip(dataset[16])
-			y = np.random.choice(stuff[0], p=stuff[1])
-			place.append(y.lower())
-			x = y 
-			
-		if char == 'R':
-			stuff = data_strip(dataset[17])
-			y = np.random.choice(stuff[0], p=stuff[1])
-			place.append(y.lower())
-			x = y
-			
-		if char == 'S':
-			stuff = data_strip(dataset[18])
-			y = np.random.choice(stuff[0], p=stuff[1])
-			place.append(y.lower())
-			x = y  
-			
-		if char == 'T':
-			stuff = data_strip(dataset[19])
-			y = np.random.choice(stuff[0], p=stuff[1])
-			place.append(y.lower())
-			x = y
-		
-		if char == 'U':
-			stuff = data_strip(dataset[20])
-			y = np.random.choice(stuff[0], p=stuff[1])
-			place.append(y.lower())
-			x = y   
-			
-		if char == 'V':
-			stuff = data_strip(dataset[21])
-			y = np.random.choice(stuff[0], p=stuff[1])
-			place.append(y.lower())
-			x = y 
-			
-		if char == 'W':
-			stuff = data_strip(dataset[22])
-			y = np.random.choice(stuff[0], p=stuff[1])
-			place.append(y.lower())
-			x = y   
-			
-		if char == 'X':
-			stuff = data_strip(dataset[23])
-			y = np.random.choice(stuff[0], p=stuff[1])
-			place.append(y.lower())
-			x = y      
-			
-		if char == 'Y':
-			stuff = data_strip(dataset[24])
-			y = np.random.choice(stuff[0], p=stuff[1])
-			place.append(y.lower())
-			x = y       
-			
-		if char == 'Z':
-			stuff = data_strip(dataset[25])
-			y = np.random.choice(stuff[0], p=stuff[1])
-			place.append(y.lower())
-			x = y       
-			
-		if char == ' ':
-			stuff = data_strip(dataset[26])
-			y = np.random.choice(stuff[0], p=stuff[1])
-			place.append(y.upper())
-			x = y 
-		final_word = "".join(place)
-		return final_word
+        if x == 'A':
+            stuff = data_strip(dataset[0])
+            y = np.random.choice(stuff[0], p=stuff[1])
+            place.append(y.lower())
+            x = y
+        
+        if x == 'B':
+            stuff = data_strip(dataset[1])
+            y = np.random.choice(stuff[0], p=stuff[1])
+            place.append(y.lower())
+            x = y
+                
+        if x == 'C':
+            stuff = data_strip(dataset[2])
+            y = np.random.choice(stuff[0], p=stuff[1])
+            place.append(y.lower())
+            x = y
+            
+        if x == 'D':
+            stuff = data_strip(dataset[3])
+            y = np.random.choice(stuff[0], p=stuff[1])
+            place.append(y.lower())
+            x = y
+            
+        if x == 'E':
+            stuff = data_strip(dataset[4])
+            y = np.random.choice(stuff[0], p=stuff[1])
+            place.append(y.lower())
+            x = y
+        
+        if x == 'F':
+            stuff = data_strip(dataset[5])
+            y = np.random.choice(stuff[0], p=stuff[1])
+            place.append(y.lower())
+            x = y
+            
+        if x == 'G':
+            stuff = data_strip(dataset[6])
+            y = np.random.choice(stuff[0], p=stuff[1])
+            place.append(y.lower())
+            x = y
+            
+        if x == 'H':
+            stuff = data_strip(dataset[7])
+            y = np.random.choice(stuff[0], p=stuff[1])
+            place.append(y.lower())
+            x = y
+            
+        if x == 'I':
+            stuff = data_strip(dataset[8])
+            y = np.random.choice(stuff[0], p=stuff[1])
+            place.append(y.lower())
+            x = y
+            
+        if x == 'J':
+            stuff = data_strip(dataset[9])
+            y = np.random.choice(stuff[0], p=stuff[1])
+            place.append(y.lower())
+            x = y
+        
+        if x == 'K':
+            stuff = data_strip(dataset[10])
+            y = np.random.choice(stuff[0], p=stuff[1])
+            place.append(y.lower())
+            x = y
+            
+        if x == 'L':
+            stuff = data_strip(dataset[11])
+            y = np.random.choice(stuff[0], p=stuff[1])
+            place.append(y.lower())
+            x = y
+            
+        if x == 'M':
+            stuff = data_strip(dataset[12])
+            y = np.random.choice(stuff[0], p=stuff[1])
+            place.append(y.lower())
+            x = y
+            
+        if x == 'N':
+            stuff = data_strip(dataset[13])
+            y = np.random.choice(stuff[0], p=stuff[1])
+            place.append(y.lower())
+            x = y      
+        
+        if x == 'O':
+            stuff = data_strip(dataset[14])
+            y = np.random.choice(stuff[0], p=stuff[1])
+            place.append(y.lower())
+            x = y
+            
+        if x == 'P':
+            stuff = data_strip(dataset[15])
+            y = np.random.choice(stuff[0], p=stuff[1])
+            place.append(y.lower())
+            x = y
+            
+        if x == 'Q':
+            stuff = data_strip(dataset[16])
+            y = np.random.choice(stuff[0], p=stuff[1])
+            place.append(y.lower())
+            x = y 
+            
+        if x == 'R':
+            stuff = data_strip(dataset[17])
+            y = np.random.choice(stuff[0], p=stuff[1])
+            place.append(y.lower())
+            x = y
+            
+        if x == 'S':
+            stuff = data_strip(dataset[18])
+            y = np.random.choice(stuff[0], p=stuff[1])
+            place.append(y.lower())
+            x = y  
+            
+        if x == 'T':
+            stuff = data_strip(dataset[19])
+            y = np.random.choice(stuff[0], p=stuff[1])
+            place.append(y.lower())
+            x = y
+        
+        if x == 'U':
+            stuff = data_strip(dataset[20])
+            y = np.random.choice(stuff[0], p=stuff[1])
+            place.append(y.lower())
+            x = y   
+            
+        if x == 'V':
+            stuff = data_strip(dataset[21])
+            y = np.random.choice(stuff[0], p=stuff[1])
+            place.append(y.lower())
+            x = y 
+            
+        if x == 'W':
+            stuff = data_strip(dataset[22])
+            y = np.random.choice(stuff[0], p=stuff[1])
+            place.append(y.lower())
+            x = y   
+            
+        if x == 'X':
+            stuff = data_strip(dataset[23])
+            y = np.random.choice(stuff[0], p=stuff[1])
+            place.append(y.lower())
+            x = y      
+            
+        if x == 'Y':
+            stuff = data_strip(dataset[24])
+            y = np.random.choice(stuff[0], p=stuff[1])
+            place.append(y.lower())
+            x = y       
+            
+        if x == 'Z':
+            stuff = data_strip(dataset[25])
+            y = np.random.choice(stuff[0], p=stuff[1])
+            place.append(y.lower())
+            x = y       
+            
+        if x == ' ':
+            stuff = data_strip(dataset[26])
+            y = np.random.choice(stuff[0], p=stuff[1])
+            place.append(y.upper())
+            x = y 
+        final_word = "".join(place)
+        return final_word
     
+def test(language):
+    answer = raw_input("Do you want to generate a name?\n")
+    if answer == "y":
+        print generate_name(language)
+        test(language)
+    else:
+        exit()
+
 def main():
-	working_dir = os.path.dirname(os.path.abspath( __file__ ))
-    culture_path = working_dir + "\cultures"
+    working_dir = os.path.dirname(os.path.abspath( __file__ ))
+    culture_path = "{0}\cultures".format(working_dir)
     print os.listdir(culture_path)
     file_path = raw_input('Which culture do you wish to generate names from? \nPlease type the name exactly.\n')
-	language = import_data(file_path)
-	
-	
+    final_path = "{0}\{1}".format(culture_path, file_path)
+    language = import_data(final_path)
+    test(language)
+    
+    
 main()
-	
-			
